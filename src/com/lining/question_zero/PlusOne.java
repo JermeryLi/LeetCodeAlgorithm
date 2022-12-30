8a0ce3bc84161f8a62313eb2a9900c4a7772a7df
lining <liningjer@163.com>
2022-12-30 14:45:57 +0800

    add leetcode 66

---
+++ src/com/lining/question_zero/PlusOne.java
@@ -0,0 +1,17 @@
+package com.lining.question_zero;
+
+/**
+ * 描述：
+ *
+ * @author lining
+ * createTime 2022/12/30 13:58
+ * lastModify
+ * lastModifyTime
+ * 小组
+ */
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+
+
+    }
+}
