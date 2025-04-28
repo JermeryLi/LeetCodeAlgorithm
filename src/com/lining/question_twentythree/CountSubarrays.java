fd7b30b7323ef5b42ed7b2d65383edde65965506
lining <liningjer@163.com>
2025-04-28 15:52:11 +0800

    add FindClosest.java、MinOperations.java、SubarraySum.java

---
+++ src/com/lining/question_twentythree/CountSubarrays.java
@@ -0,0 +1,20 @@
+package com.lining.question_twentythree;
+
+/***
+ * @author lining
+ * @description
+ * @create 2025/4/28 15:23
+ * https://leetcode.cn/problems/count-subarrays-with-score-less-than-k/?envType=daily-question&envId=2025-04-28
+ * 2302. 统计得分小于 K 的子数组数目
+ * 一个数组的 分数 定义为数组之和 乘以 数组的长度。
+ *
+ * 比方说，[1, 2, 3, 4, 5] 的分数为 (1 + 2 + 3 + 4 + 5) * 5 = 75 。
+ * 给你一个正整数数组 nums 和一个整数 k ，请你返回 nums 中分数 严格小于 k 的 非空整数子数组数目。
+ *
+ * 子数组 是数组中的一个连续元素序列。
+ **/
+public class CountSubarrays {
+    public long countSubarrays(int[] nums, long k) {
+        return 0;
+    }
+}
