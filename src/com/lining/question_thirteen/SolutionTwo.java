cdc8afba7774f18d9a436e030c71d0de5022fa51
JermeryLi <liningjer@163.com>
2025-12-08 22:53:09 +0800

    add SolutionTwo.java

---
+++ src/com/lining/question_thirteen/SolutionTwo.java
@@ -0,0 +1,37 @@
+package com.lining.question_thirteen;
+
+import com.lining.TreeNode;
+
+
+/***
+ * @author lining
+ * @time 2025/12/8 22:47
+ * https://leetcode.cn/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/description/
+ * 1379. 找出克隆二叉树中的相同节点
+ *
+ * 给你两棵二叉树，原始树 original 和克隆树 cloned，以及一个位于原始树 original 中的目标节点 target。
+ * 其中，克隆树 cloned 是原始树 original 的一个 副本 。
+ * 请找出在树 cloned 中，与 target 相同 的节点，并返回对该节点的引用（在 C/C++ 等有指针的语言中返回 节点指针，
+ * 其他语言返回节点本身）。
+ *
+ * 注意：你 不能 对两棵二叉树，以及 target 节点进行更改。只能 返回对克隆树 cloned 中已有的节点的引用。
+ *
+ * 树中节点的数量范围为 [1, 104] 。
+ * 同一棵树中，没有值相同的节点。
+ * target 节点是树 original 中的一个节点，并且不会是 null 。
+ **/
+public class SolutionTwo {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if (original == null) {
+            return null;
+        }
+        if (original == target) {
+            return cloned;
+        }
+        TreeNode left = getTargetCopy(original.left, cloned.left, target);
+        if (left != null) {
+            return left;
+        }
+        return getTargetCopy(original.right, cloned.right, target);
+    }
+}
