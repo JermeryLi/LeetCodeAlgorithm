f0d84bb85c1ad2de1ade3288a53ad49ebf7cc38c
JermeryLi <liningjer@163.com>
2025-12-12 22:49:29 +0800

    add CountPoints.java

--- src/com/lining/question_eighteen/CountPoints.java
+++ src/com/lining/question_eighteen/CountPoints.java
@@ -14,6 +14,18 @@ package com.lining.question_eighteen;
  */
 public class CountPoints {
     public int[] countPoints(int[][] points, int[][] queries) {
-        return null;
+        int m = points.length, n = queries.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; ++i) {
+            int cx = queries[i][0], cy = queries[i][1], cr = queries[i][2];
+            for (int[] point : points) {
+                int px = point[0], py = point[1];
+                if ((cx - px) * (cx - px) + (cy - py) * (cy - py) <= cr * cr) {
+                    ++ans[i];
+                }
+            }
+        }
+        return ans;
     }
 }
+
